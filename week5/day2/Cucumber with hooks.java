f17869576ff72cbe46972f60612384593deec4a6
TestLeafPages <earth@testleaf.com>
2023-05-08 17:52:59 +0530

    Add files via upload

---
+++ week5/day2/Cucumber with hooks.java
@@ -0,0 +1,8 @@
+Assignments on cucumber
+===============
+1.Implement tags for the test cases (@functional, @regression, @smoke and @sanity)
+2. Run all the @smoke and @snaity test scenarios using tags attribute in the @CucumberOptions
+
+3.Implement the preconditions and postconditions using *Hooks Implementation and extend baseclass for all 5 testcases
+
+Implement all your learnings with salesforce and service now testcases
---
+++ week5/day4/POM.2.java
@@ -0,0 +1,9 @@
+
+ Data parametrization
+ ========================
+           *Apply  for all the leaftaps testcases , wherever applicable
+    - Create new excel document for the testcases that require the multiple data to operate on.
+    - Implement the Data parametrization for all the required testcases through the DataProvider class to read the data from the excel sheet
+
+==================================================================
+Apply all the today's learning in the Salesforce application
