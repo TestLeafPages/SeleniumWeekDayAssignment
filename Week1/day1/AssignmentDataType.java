4a7955af531de19ee1d7820514c335adecb4925d
TestLeafPages <earth@testleaf.com>
2023-07-17 17:53:25 +0530

    Update AssignmentDataType.java

--- Week1/day1/AssignmentDataType.java
+++ Week1/day1/AssignmentDataType.java
@@ -1,5 +1,14 @@
 package week1.day1;
 
 public class AssignmentDataType {
-
+//	Assignment:
+//		a) Create a class by name: Firefox (with main method) 
+//b) Create following variables 
+//1) version = 100.2  (use dataType float) 
+//2) name = "firefox" (use dataType String)
+//3) visible = true (use dataType boolean)
+//4) year = 1998 (use dataType int)
+//5) logo = 'm' (Use datatype char)
+//c) print all data types statement
+//d) run and confirm it in console
 }
