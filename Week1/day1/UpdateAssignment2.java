2e54fe542a898498826cea6c0e15d0990a90801e
Dilip2005 <101985281+Dilip2005@users.noreply.github.com>
2023-08-14 12:30:55 +0530

    Update and rename Assignment2.java to UpdateAssignment2.java

--- Week1/day1/UpdateAssignment2.java
+++ Week1/day1/UpdateAssignment2.java
@@ -2,5 +2,5 @@
 - Create a  new class as "Iphone" under week1.day1
 - Create 2 methods (makeCall() , model()) with simple print statement
 - Create main method in another Class as SmartPhone
-- Create object for Mobile class and call the methods using the object
+- Create object for Iphone class and call the methods using the object
 - Execute the class and get the result in console
